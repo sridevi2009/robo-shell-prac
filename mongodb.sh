ceccc03860fc093dd6cdba407df2ea3c3dd57e0d
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-31 20:01:05 +0530

    roles

--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,21 @@
+# SED editor: streamline editor
+# cp /etc/passwd passwd
+# cat passwd
+# sed -e '1 a I am learning shell script'<file name>
+# sed -e '1 a i am learning shell script' passwd
+# sed -e '2 a I am learning shell script' passwd
+# temporary editor= -e
+# permanant editor= -i
+# sed -i '1 a im learning shell script' passwd
+# sed -i '1 i Im leaning shell script' passwd
+# here i indicates= insert, which ia going to print this line before 1st line
+# sed -e 's/word-to-find/word-to-replace/'
+# sed -e 's/sbin/SBIN/' passwd {s=substitution} to replace the word
+# sed -e 's/sbin/SBIN/g' passwd {g= will replace all words in that file}
+# sed -e '1d' passwd {to delete the lines}
+# sed -e '/learning/ d' passwd {to delte particular word in content}
+
+
 #!/bin/bash
 ID=$(id -u)
 R="\e[31m"
@@ -30,3 +48,23 @@ fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "copied mongo repo"
+
+dnf install mongodb-org -y  &>> $LOGFILE
+
+VALIDATE $? "install mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "enable mongod"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "starting mongd"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "remote access to server"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "restarting mongod"
