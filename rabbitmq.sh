5802b140e20f81d49599d12c388ad8ae043b4351
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-04-01 02:06:23 +0530

    roles

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -26,3 +26,31 @@ then
 else
     echo -e "$Y you are root user $N"
 fi
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "configuring yum repos"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "configure yum repos for rabbitmq"
+
+dnf install rabbitmq-server -y &>> $LOGFILE
+
+VALIDATE $? "installing rabbitmq"
+
+systemctl enable rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "enabling rabbitmq"
+
+systemctl start rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "starting rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+
+VALIDATE $? "creating roboshop user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
+
+VALIDATE $? "set permissions to roboshop"
