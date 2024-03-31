a526349f7d97e9a585a9553c3a2df9705fa695ef
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-03-31 20:03:02 +0530

    roles

--- mongodb.sh
+++ mongodb.sh
@@ -42,7 +42,7 @@ then
     echo -e "$R ERROR: Please run this script with root access $N"
     exit 1
 else
-    echo "$Y you are root user $N"
+    echo -e "$Y you are root user $N"
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
