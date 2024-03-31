31e1b80d089fd9b378d0b7af37db511be91438cb
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-04-01 02:14:33 +0530

    roles

--- payment.sh
+++ payment.sh
@@ -26,3 +26,53 @@ then
 else
     echo -e "$Y you are root user $N"
 fi
+
+dnf install python36 gcc python3-devel -y &>> $LOGFILE
+
+VALIDATE $? "installing python"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "creation roboshop"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi    
+
+mkdir -p /app  &>> $LOGFILE
+
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>> $LOGFILE
+
+VALIDATE $? "downloading application code"
+
+cd /app &>> $LOGFILE
+
+unzip -o /tmp/payment.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping shipping"
+
+pip3.6 install -r requirements.txt &>> $LOGFILE
+
+VALIDATE $? "installing python dependencies"
+
+#use absolute path because payment.service exists there, we cloned this payment.service in host
+
+cp /home/centos/robo-shell-prac/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
+
+VALIDATE $? "copying systemd service"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "daemon-reload"
+
+systemctl enable payment &>> $LOGFILE
+
+VALIDATE $? "enabling payment"
+
+systemctl start payment &>> $LOGFILE
+
+VALIDATE $? "starting payment"
+
